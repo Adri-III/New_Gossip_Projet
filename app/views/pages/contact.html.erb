c913c389c6b17b3dde7c1fe09e1ec26735b05293
Tepepeka <jeremy.bourgoin@live.fr>
2021-05-13 15:07:52 +0200

    first commit

---
+++ app/views/pages/contact.html.erb
@@ -0,0 +1,1 @@
+<p>PAGE CONTACT</p>
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+  
+  root to: 'pages#home' 
+
+  get '/welcome', to: 'pages#welcome'
+  get '/team', to: 'pages#team'
+  get '/contact', to: 'pages#contact'
+
+  resources :posts
+
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
