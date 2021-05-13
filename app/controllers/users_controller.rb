9689453cc4a34bdd178d5e9ba97dd0ec405c1df7
Adri-III <trrs.adrien@gmail.com>
2021-05-14 00:36:39 +0200

    Projet Validant - Ruby on Rails: Final_Gossip_Project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,32 @@
 class UsersController < ApplicationController
-    
+
   def index
     @users = User.all
   end
-
+    
   def new
     @user = User.new
   end
-
+  
   def show
     @user = User.find(params[:id])
   end
 
   def create
-    @user = User.new(post_params)
-    if @user.save
+    @user = User.new(user_params)
+      if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
-    else
-      render :new
-    end
+      redirect_to users_path
+      else
+      render "new"
+      end
+  end
+
+  # DELETE /posts/1
+  def destroy
+    @user= User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
   end
 
 end
@@ -27,8 +34,8 @@ end
 
 ########
 private
-########
+#######
 
-def post_params
+def user_params
   params.require(:user).permit(:first_name, :email,:password,:password_confirmation)
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  validates :title, 
-  presence: true
+#  validates :title, 
+#  presence: true
 end
